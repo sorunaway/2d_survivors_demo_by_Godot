f0790fb58b473cc63183e0d3cb584577166edcc0
sorunaway <sww5777@qq.com>
2025-05-28 20:20:43 +0800

    调整伤害闪光和跳字

--- scenes/ui/floating_text.gd
+++ scenes/ui/floating_text.gd
@@ -11,14 +11,17 @@ func start(text: String):
 	var tween = create_tween()
 	tween.set_parallel()
 	
-	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), 0.4)\
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 32), 0.4)\
 	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.chain()
 	
-	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), 0.5)\
+	tween.tween_property(self, "global_position", global_position + (Vector2.DOWN * 4), 0.8)\
 	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "scale", Vector2(0.2, 0.8), 0.5)\
+	tween.tween_property(self, "scale", Vector2(0.8, 0.8), 0.8)\
 	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.5).set_delay(0.3)\
+	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	
 	tween.chain()
 	
 	tween.tween_callback(queue_free)
