4fe58684297e37dbcf44ce60b81a5c8417a795b4
sorunaway <sww5777@qq.com>
2025-06-17 18:01:25 +0800

    meta能力卡牌界面日常更新

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -5,7 +5,9 @@ extends PanelContainer
 @onready var description_label: Label = $MarginContainer/VBoxContainer/DescriptionLabel
 @onready var progress_bar: ProgressBar = $MarginContainer/VBoxContainer/VBoxContainer/ProgressBar
 @onready var purchase_button: Button = $MarginContainer/VBoxContainer/PurchaseButton
-@onready var progress_label: Label = $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/ProgressLabel
+@onready var progress_label: Label = %ProgressLabel
+@onready var count_label: Label = %CountLabel
+
 
 var upgrade: MetaUpgrade
 
@@ -25,12 +27,21 @@ func set_meta_upgrade(upgrade: MetaUpgrade):
 
 
 func update_progress():
+	var current_quantity = 0
+	if MetaProgression.save_date["meta_upgrades"].has(upgrade.id):
+		current_quantity = MetaProgression.save_date["meta_upgrades"][upgrade.id]["quantity"]
+	
+	var is_maxed = current_quantity >= upgrade.max_quantity
 	var currency = MetaProgression.save_date["meta_upgrade_currency"]
 	var percent = currency / upgrade.experience_cost
 	percent = min(percent, 1)
 	progress_bar.value = percent
-	purchase_button.disabled = percent < 1
+	purchase_button.disabled = percent < 1 || is_maxed
+	if is_maxed:
+		purchase_button.text = "已升满"
 	progress_label.text = str(currency) + "/" + str(upgrade.experience_cost)
+	
+	count_label.text = "等级: %d" % current_quantity
 
 
 func select_card():
@@ -50,5 +61,6 @@ func on_purchase_button_pressed():
 	MetaProgression.add_meta_upgrade(upgrade)
 	MetaProgression.save_date["meta_upgrade_currency"] -= upgrade.experience_cost
 	MetaProgression.save()
+	#调用所有某组的某方法
 	get_tree().call_group("meta_upgrade_card", "update_progress")
 	$AnimationPlayer.play("selected")
