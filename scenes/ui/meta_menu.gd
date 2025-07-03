ccd827734a720cf4609e8a0dce35495ec4673c22
sorunaway <sww5777@qq.com>
2025-07-03 14:58:05 +0800

    Revert "BUG修复"
    This reverts commit 8cbb1b20fe4c1037cd026ed2f6d4b630288097e5.

--- scenes/ui/meta_menu.gd
+++ scenes/ui/meta_menu.gd
@@ -10,12 +10,12 @@ var meta_upgrade_card_scene = preload("res://scenes/ui/meta_upgrade_card.tscn")
 
 
 func _ready() -> void:
-	back_button.pressed.connect(on_back_button_pressed)
 	for upgrade in upgrades:
 		var meta_upgrade_card_instance = meta_upgrade_card_scene.instantiate()
 		grid_container.add_child(meta_upgrade_card_instance)
 		meta_upgrade_card_instance.set_meta_upgrade(upgrade)
+		back_button.pressed.connect(on_back_pressed)
 
 
-func on_back_button_pressed():
+func on_back_pressed():
 	ScreenTransition.transition_to_scene("res://scenes/ui/main_menu.tscn")
