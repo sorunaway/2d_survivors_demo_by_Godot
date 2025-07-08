90aa72fae228e99a5dce5ea1ea3d06bef10edfa4
sorunaway <sww5777@qq.com>
2025-07-08 19:04:22 +0800

    update

---
+++ scenes/ability/rock_ability/dart_ability.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var level = 1
+var hp = 1
+var speed = 100
+var damage = 5
+var knock_amount = 100
+var attack_size = 1.0
+
+var target = Vector2.ZERO
+var angle = Vector2.ZERO
+
+
+func _ready() -> void:
+	angle = global_position.direction_to(target)
+	rotation = angle.angle()
+	match level:
+		1:
+			hp = 1
+			speed = 100
+			damage = 5
+			knock_amount = 100
+			attack_size = 1.0
+
+
+func _physics_process(delta: float) -> void:
+	position += angle * speed * delta
+	
+
+func enemy_hit(charge = 1):
+	hp -= charge
+	if hp <= 0:
+		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
